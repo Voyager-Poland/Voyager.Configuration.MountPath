e90f337adeb47c69d246f79baeddc4f21a8ad0d1
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2023-04-11 22:10:22 +0200

    poprawa skryptu

--- scripts/PublishNuget.ps1
+++ scripts/PublishNuget.ps1
@@ -1,8 +1,8 @@
-$version='1.2.0'
+$version='1.0.0'
 dotnet build -c Release   /property:Version=$version
 dotnet pack -c Release /property:Version=$version
 
-$ostatniPakiet = (gci .\src\Voyager.HealthEndpoint\bin\Release\*.nupkg | select -last 1).Name
-$sciezka = ".\src\Voyager.HealthEndpoint\bin\Release\$ostatniPakiet"
+$ostatniPakiet = (gci .\src\Voyager.Configuration.MountPath\bin\Release\*.nupkg | select -last 1).Name
+$sciezka = ".\src\Voyager.Configuration.MountPath\bin\Release\$ostatniPakiet"
 
 dotnet nuget push "$sciezka" -s Voyager-Poland

3ec40f739d1c87606653a118539e10146d1f42ec
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2024-06-06 13:06:54 +0200

    Encription tools

---
+++ src/Voyager.Configuration.Encrypt/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args.Length == 0)
+{
+	Console.Write("Param is requeried");
+	return;
+}
+
+var encodeKey = Environment.GetEnvironmentVariable("ENCODE_KEY");
+if (string.IsNullOrEmpty(encodeKey))
+{
+	Console.Write("Lack of enviromentVariable ENCODE_KEY");
+	return;
+}
+
+Voyager.Configuration.MountPath.Encryption.Encryptor encryptor = new Voyager.Configuration.MountPath.Encryption.Encryptor(encodeKey);
+
+Console.WriteLine($"To encrypt{args[0]}");
+
+Console.WriteLine(encryptor.Encrypt(args[0]));

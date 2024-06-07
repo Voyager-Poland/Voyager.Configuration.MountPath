72bd9e75358463545cdf3d7d8a1dfa983bfc4704
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2024-06-07 14:59:35 +0200

    Program to decode

--- src/Voyager.Configuration.MountPath/Encryption/Encryptor.cs
+++ src/Voyager.Configuration.MountPath/Encryption/Encryptor.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace Voyager.Configuration.MountPath.Encryption
 {
-	internal class Encryptor
+	public class Encryptor
 	{
-		byte[] keyBytes;
-		byte[] ivBytes;
+		readonly byte[] keyBytes;
+		readonly byte[] ivBytes;
 		public Encryptor(string key)
 		{
 			keyBytes = Encoding.ASCII.GetBytes(key.Substring(0, 8));
@@ -17,34 +15,14 @@ namespace Voyager.Configuration.MountPath.Encryption
 
 		public string Encrypt(string dataParam)
 		{
-			using DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
-			var transform = cryptoProvider.CreateEncryptor(keyBytes, ivBytes);
-			using var ms = new MemoryStream();
-			using var crypstream = new CryptoStream(ms, transform, CryptoStreamMode.Write);
-			using var sw = new StreamWriter(crypstream);
-			sw.Write(dataParam);
-			sw.Close();
-			crypstream.Close();
-			return Convert.ToBase64String(ms.ToArray());
+			using CoreEncoder coreEncoder = new CoreEncoder(keyBytes, ivBytes);
+			return Convert.ToBase64String(coreEncoder.Encrypt(dataParam));
 		}
-
 
 		public string Decrypt(string dataParamtxt)
 		{
-			using DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
-			var result = string.Empty;
-			using var ms = new MemoryStream(Convert.FromBase64String(dataParamtxt));
-			using (var crypstream = new CryptoStream(ms, cryptoProvider.CreateDecryptor(keyBytes, ivBytes), CryptoStreamMode.Read))
-			{
-				using (var sr = new StreamReader(crypstream))
-				{
-					result = sr.ReadToEnd();
-					sr.Close();
-				}
-				crypstream.Close();
-			}
-			ms.Close();
-			return result;
+			using CoreEncoder coreEncoder = new CoreEncoder(keyBytes, ivBytes);
+			return coreEncoder.Decrypt(Convert.FromBase64String(dataParamtxt));
 		}
 	}
 }
